b13743f871ca4567ab3e35f1050648cfb0f26a1f
Andross Moonah <amoonah@jahia.com>
2016-08-29 08:37:19 -0400

    Wip, but I think it's done

--- src/assets/js/angularSeedModule.js
+++ src/assets/js/angularSeedModule.js
@@ -1,4 +1,7 @@
-angular.module("AngularSeed", [])
-        .controller("index", ["$scope", function($scope) {
-            $scope.inputMessage;
-    }]);
+(function() {
+    'use strict'; // A mode introduced to the browser to provide a safe layer.
+
+    angular.module("AngularSeed", [])
+
+})();
+
